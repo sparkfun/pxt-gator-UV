4c8535b7206acf1287f20cbe961d333f3b57baae
Andrew England <andrew.england@sparkfun.local>
2019-04-22 15:03:06 -0600

    initial commit

---
+++ enums.d.ts
@@ -0,0 +1,5 @@
+// Auto-generated. Do not edit.
+declare namespace gatorUV {
+}
+
+// Auto-generated. Do not edit. Really.
---
+++ gatoruv.ts
@@ -0,0 +1,83 @@
+/**
+* Andy England @ SparkFun Electronics
+* September 6, 2018
+
+* Development environment specifics:
+* Written in Microsoft Makecode
+* Tested with a SparkFun gatorUV sensor and micro:bit
+*
+* This code is released under the [MIT License](http://opensource.org/licenses/MIT).
+* Please review the LICENSE.md file included with this example. If you have any questions
+* or concerns with licensing, please contact techsupport@sparkfun.com.
+* Distributed as-is; no warranty is given.
+*/
+
+
+/**
+ * Functions to operate the gatorUV sensor
+ */
+
+ enum gatorUVRefreshTime{
+	 half=1,
+	 one=2,
+	 two=3,
+	 four=4,
+ }
+
+//% color=#f44242 icon="\uf185"
+namespace gatorUV {
+	let highAddress = 0x39
+    let lowAddress = 0x38
+    let araAddress = 0x0C
+	let commandRegister = 0x06
+    // Functions for reading UV from the gatorUV in UV or straight adv value
+
+	function clearAck ()
+	{
+		pins.i2cReadNumber(araAddress, NumberFormat.UInt8LE)
+	}
+	
+	function writeCommand()
+	{
+		pins.i2cWriteNumber(lowAddress, commandRegister)
+	}
+	
+    //% weight=30 blockId="gatorUV_begin" block="Initialize gator:UV sensor"
+	export function begin() 
+	{
+		commandRegister = 0x06
+		clearAck()
+		writeCommand()
+	}
+    /**
+    * Reads the number
+    */
+    //% weight=30 blockId="gatorUV_IT" block="Set refresh time on UV Sensor to %gatorUVRefreshTime"
+    export function IT(type: gatorUVRefreshTime): void{
+	  commandRegister = 0x02
+      switch(type){
+        case gatorUVRefreshTime.half: 
+			commandRegister | (0b00 << 2);
+			break;
+        case gatorUVRefreshTime.one: 
+			commandRegister | (0b01 << 2);
+			break;
+        case gatorUVRefreshTime.two: 
+			commandRegister | (0b10 << 2);
+			break;
+        case gatorUVRefreshTime.four: 
+			commandRegister | (0b11 << 2);
+			break;
+        default: return -11111111
+      }
+	  writeCommand();
+    }
+}
+
+
+    //% weight=30 blockId="gatorUV_UV" block="Get UVA Reading"
+    export function UV(): number{
+		let msb = pins.i2cReadNumber(highAddress, UInt8LE)
+		let lsb = pins.i2cReadNumber(lowAddress, UInt8LE)
+		return (msb << 8) | lsb
+	}
